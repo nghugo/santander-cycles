f75adc3c58ce966bbdf2fd08d7da467882a87f2b
nghugo <nghugoryan@gmail.com>
2023-09-01 01:09:21 +0800

    add autocomplete to update state array

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,30 +5,77 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [autocompleteResponse, setAutocompleteResponse] = useState("");
+  const [autocompleteValues, setAutocompleteValues] = useState([]);
+  const fullnames = autocompleteValues
+    ? autocompleteValues.map((entry) => entry.fullname)
+    : [];
 
   useEffect(() => {
     // no need to fetch if search term is empty
     if (searchTerm) {
-        // debounce -> fetch after user has finished typing, rather than on every keystroke
-        const delayDebounceFn = setTimeout(() => {
-          console.log(`* ${searchTerm} *`);
-          fetch(
-            "https://api.locationiq.com/v1/autocomplete?" +
-              new URLSearchParams({
-                //  key: import.meta.env.VITE_LOCATION_IQ_API_KEY,
-                countrycodes: "gb",
-                format: "json",
-                q: { searchTerm },
-              })
-          )
-            .then((response) => {
-              console.log(response); // TO IMPLEMENT: code for saving fetched results
-              console.log(response.text());
+      // debounce -> fetch after user has finished typing, rather than on every keystroke
+      const delayDebounceFn = setTimeout(() => {
+        console.log(`* ${searchTerm} *`);
+
+        fetch(
+          "https://api.locationiq.com/v1/autocomplete?" +
+            new URLSearchParams({
+                key: import.meta.env.VITE_LOCATION_IQ_API_KEY,
+              countrycodes: "gb",
+              format: "json",
+              q: searchTerm,
+              limit: "6",
             })
-            .catch((error) => console.error(error));
-        }, 3000);
-        return () => clearTimeout(delayDebounceFn);
+        )
+          .then((response) => {
+            if (!response.ok) {
+              let err = new Error("HTTP status code: " + response.status);
+              err.response = response;
+              err.status = response.status;
+              throw err;
+            }
+            return response.json();
+          })
+          .then((responseArray) => {
+            console.log(responseArray);
+            console.log("------");
+
+            const responseArrayExtracted = responseArray.map((entry) => ({
+              lat: Number(entry.lat),
+              lng: Number(entry.lon),
+              fullname: entry.display_name,
+              addressObj: entry.address,
+            }));
+            setAutocompleteValues(responseArrayExtracted);
+            // console.log(responseArrayExtracted);
+            // console.log(typeof responseArrayExtracted);
+          })
+          .catch((error) => console.error(error));
+
+        //   {
+        //     const responseArray = response.json()
+        //     console.log(responseArray);
+        //     console.log("------")
+        //     // console.log(typeof(responseArray))
+        //     if (response.status == 200) {
+        //       const responseArrayExtracted = Object.keys(responseArray).map((key) => [
+        //         key,
+        //         responseArray[key],
+        //       ]);
+        //       setAutocompleteValues(
+        //         responseArrayExtracted.map((entry) => ({
+        //           lat: Number(entry.lat),
+        //           lng: Number(entry.lon),
+        //           fullname: entry.display_name,
+        //           addressObj: entry.address,
+        //         }))
+        //       );
+        //       console.log(responseArrayExtracted)
+        //       console.log(typeof(responseArrayExtracted))
+        //     }
+        //   }
+      }, 3000);
+      return () => clearTimeout(delayDebounceFn);
     }
   }, [searchTerm]);
 
@@ -36,7 +83,8 @@ const SearchBar = () => {
     <Autocomplete
       id="search-bar"
       freeSolo
-      options={["abc", "def"]}
+      //   options={["abc", "def"]}
+      options={fullnames} // To implement: style options differenly than actual value
       onInputChange={(e, value) => {
         setSearchTerm(value);
       }}
