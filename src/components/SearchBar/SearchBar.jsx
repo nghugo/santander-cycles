52aad24a27e1250ff0b2d99869df266cf2bef1a6
nghugo <nghugoryan@gmail.com>
2023-08-31 22:54:37 +0800

    add debounce to autocomplete search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,25 +4,33 @@ import { InputAdornment, TextField, Autocomplete } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
-  const [searchString, setSearchString] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   const [autocompleteResponse, setAutocompleteResponse] = useState("");
 
-  //     useEffect(() => {
-  //     fetch(
-  //       "https://api.locationiq.com/v1/autocomplete?" +
-  //         new URLSearchParams({
-  //             // key: import.meta.env.VITE_LOCATION_IQ_API_KEY,
-  //           countrycodes: "gb",
-  //           q: {searchString},  // To implement
-  //         })
-  //     )
-  //       .then((response) => {
-  //         console.log(response); // TO IMPLEMENT: code for saving fetched results
-  //         console.log(response.text());
-  //       })
-  //       .catch((error) => console.error(error));
-
-  //   }, []); // TO implement
+  useEffect(() => {
+    // no need to fetch if search term is empty
+    if (searchTerm) {
+        // debounce -> fetch after user has finished typing, rather than on every keystroke
+        const delayDebounceFn = setTimeout(() => {
+          console.log(`* ${searchTerm} *`);
+          fetch(
+            "https://api.locationiq.com/v1/autocomplete?" +
+              new URLSearchParams({
+                //  key: import.meta.env.VITE_LOCATION_IQ_API_KEY,
+                countrycodes: "gb",
+                format: "json",
+                q: { searchTerm },
+              })
+          )
+            .then((response) => {
+              console.log(response); // TO IMPLEMENT: code for saving fetched results
+              console.log(response.text());
+            })
+            .catch((error) => console.error(error));
+        }, 3000);
+        return () => clearTimeout(delayDebounceFn);
+    }
+  }, [searchTerm]);
 
   return (
     <Autocomplete
@@ -30,9 +38,9 @@ const SearchBar = () => {
       freeSolo
       options={["abc", "def"]}
       onInputChange={(e, value) => {
-        setSearchString(value);
+        setSearchTerm(value);
       }}
-      value={searchString || ""}
+      value={searchTerm || ""}
       renderInput={(params) => {
         return (
           <TextField
@@ -49,8 +57,8 @@ const SearchBar = () => {
             sx={{
               width: "100%",
             }}
-            error={() => true}
-            helperText={true ? "Invalid input" : " "}
+            error={true} // To Implement boolean
+            helperText={true ? "Invalid input" : " "} // To Implement boolean
           />
         );
       }}
